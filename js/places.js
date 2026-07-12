ea6a795c3f4bb60479289b679804c3f81342520f
arche <arche1206@naver.com>
2026-07-13 03:23:03 +0900

    Update places.js

--- js/places.js
+++ js/places.js
@@ -7,7 +7,8 @@ const places = [
         address: "제주특별자치도 제주시 공항로 2",
         time: "05:00 ~ 23:00",
         price: "무료",
-        description: "제주도의 대표 국제공항으로 제주 여행의 시작점입니다."
+        description: "제주도의 대표 국제공항으로 제주 여행의 시작점입니다.",
+        link: "https://www.airport.co.kr/jeju/index.do"
     },
     {
         id: 2,
@@ -17,7 +18,8 @@ const places = [
         address: "제주특별자치도 제주시 1100로 3198-8",
         time: "10:00 ~ 18:00 (월요일 휴관)",
         price: "성인 14,000원",
-        description: "컴퓨터와 게임의 역사를 체험할 수 있는 국내 최초의 컴퓨터 박물관입니다."
+        description: "컴퓨터와 게임의 역사를 체험할 수 있는 국내 최초의 컴퓨터 박물관입니다.",
+        link: "https://www.nexonmuseum.org/ko"
     },
     {
         id: 3,
@@ -27,7 +29,8 @@ const places = [
         address: "제주특별자치도 서귀포시 안덕면 녹차분재로 218",
         time: "09:00 ~ 18:00",
         price: "성인 15,500원",
-        description: "실제 항공기와 우주과학을 체험할 수 있는 국내 최대 규모의 항공우주 박물관입니다."
+        description: "실제 항공기와 우주과학을 체험할 수 있는 국내 최대 규모의 항공우주 박물관입니다.",
+        link: "https://www.jdc-jam.com/main.do"
     },
     {
         id: 4,
@@ -37,6 +40,7 @@ const places = [
         address: "제주특별자치도 서귀포시 성산읍 섭지코지로 95",
         time: "09:30 ~ 18:00",
         price: "성인 43,700원",
-        description: "국내 최대 규모의 아쿠아리움으로 다양한 해양생물을 만날 수 있습니다."
+        description: "국내 최대 규모의 아쿠아리움으로 다양한 해양생물을 만날 수 있습니다.",
+        link: "https://www.aquaplanet.co.kr/jeju/index.do"
     }
 ];
